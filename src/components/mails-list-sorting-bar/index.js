484548f2eb3b32c74fcae4c2afd25de4bf0a2f3c
Marek Bareła <marek.barela@wp.pl>
2019-09-27 01:57:47 +0200

    sorting selection and styling commited

--- src/components/mails-list-sorting-bar/index.js
+++ src/components/mails-list-sorting-bar/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Icon from "../awesome-icon";
-import { switchAllMailsSelecting } from "../../actions/sortingBarActions";
+import {
+  switchAllMailsSelecting,
+  sortMailsByType
+} from "../../actions/sortingBarActions";
+import getSortingType from "../../selectors/getSortingType";
 import { connect } from "react-redux";
 import { faStar } from "@fortawesome/free-regular-svg-icons";
 import { faSort } from "@fortawesome/free-solid-svg-icons";
 import styles from "./index.module.css";
 
-const ListSortingBar = ({ messages, switchAllMailsSelecting }) => {
+const ListSortingBar = ({
+  messages,
+  switchAllMailsSelecting,
+  sortMailsByType,
+  sortingType
+}) => {
   const { sortingBar, checkbox, favorite, author, subject, date } = styles;
   const checkIfAllMessagesAreSelected = messages.every(
     message => message.isChecked === true
@@ -14,29 +23,50 @@ const ListSortingBar = ({ messages, switchAllMailsSelecting }) => {
   const handleSwitchAllMessages = e => {
     switchAllMailsSelecting(e.target.checked, messages);
   };
-  const activeColor = checkIfAllMessagesAreSelected && "#a80303";
+  const activeCheckboxColor = checkIfAllMessagesAreSelected && "#860101";
+  const activeSortingColor = type => sortingType === type && "#860101";
 
   return (
     <div className={sortingBar}>
-      <div className={checkbox} style={{ borderBottomColor: activeColor }}>
+      <div
+        className={checkbox}
+        style={{ borderBottomColor: activeCheckboxColor }}
+      >
         <input
           type="checkbox"
           checked={checkIfAllMessagesAreSelected}
           onChange={e => handleSwitchAllMessages(e)}
+          style={{ borderBottomColor: activeCheckboxColor }}
         />
       </div>
-      <div className={favorite}>
+      <div
+        className={favorite}
+        style={{ borderBottomColor: activeSortingColor("isFavorite") }}
+        onClick={() => sortMailsByType("isFavorite")}
+      >
         <Icon icon={faStar} />
       </div>
-      <div className={author}>
+      <div
+        className={author}
+        style={{ borderBottomColor: activeSortingColor("author") }}
+        onClick={() => sortMailsByType("author")}
+      >
         <span>Author</span>
         <Icon icon={faSort} />
       </div>
-      <div className={subject}>
+      <div
+        className={subject}
+        style={{ borderBottomColor: activeSortingColor("subject") }}
+        onClick={() => sortMailsByType("subject")}
+      >
         <span>Subject</span>
         <Icon icon={faSort} />
       </div>
-      <div className={date}>
+      <div
+        className={date}
+        style={{ borderBottomColor: activeSortingColor("date") }}
+        onClick={() => sortMailsByType("date")}
+      >
         <span>Date</span>
         <Icon icon={faSort} />
       </div>
@@ -44,11 +74,16 @@ const ListSortingBar = ({ messages, switchAllMailsSelecting }) => {
   );
 };
 
+const mapStateToProps = state => ({
+  sortingType: getSortingType(state)
+});
+
 const mapDispatchToProps = {
-  switchAllMailsSelecting
+  switchAllMailsSelecting,
+  sortMailsByType
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(ListSortingBar);
