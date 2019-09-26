71b72166251cfb8e68190481ea932206c769102b
Marek Bareła <marek.barela@wp.pl>
2019-09-27 01:38:26 +0200

    sorting update

--- src/components/mails-wrapper/index.js
+++ src/components/mails-wrapper/index.js
@@ -5,36 +5,36 @@ import getSortingType from "../../selectors/getSortingType";
 import { connect } from "react-redux";
 import styles from "./index.module.css";
 
-const MailsContainer = ({listOfMails, sortingType}) => {
+const MailsContainer = ({ listOfMails, sortingType }) => {
   const { container } = styles;
-  const [messages, setMessages] = useState([...listOfMails]);
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    sortMessages(sortingType)
-  })
+    sortMessages(sortingType);
+  });
 
-  const sortMessages = (sortingType) => {
+  const sortMessages = sortingType => {
     const sortedMessages = listOfMails.sort(compareMessagesBy(sortingType));
     setMessages(sortedMessages);
-  }
+  };
 
-  const compareMessagesBy = (type) => {
-    const fromNewToOld = false
+  const compareMessagesBy = type => {
+    const fromNewToOld = false;
     const key = type;
     if (fromNewToOld) {
-      return function (a, b) {
+      return function(a, b) {
         if (a[key] < b[key]) return -1;
         if (a[key] > b[key]) return 1;
         return 0;
-      }
+      };
     } else {
-      return function (a, b) {
+      return function(a, b) {
         if (a[key] > b[key]) return -1;
         if (a[key] < b[key]) return 1;
         return 0;
-      }
+      };
     }
-  }
+  };
 
   return (
     <section className={container}>
@@ -46,6 +46,9 @@ const MailsContainer = ({listOfMails, sortingType}) => {
 
 const mapStateToProps = state => ({
   sortingType: getSortingType(state)
-})
+});
 
-export default connect(mapStateToProps, null)(MailsContainer);
+export default connect(
+  mapStateToProps,
+  null
+)(MailsContainer);
