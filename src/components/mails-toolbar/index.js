7ebe4d3bf64673c5c0ebec1e29b0d384d9b93667
Marek Bareła <marek.barela@wp.pl>
2019-09-26 03:45:03 +0200

    toolbar structure and styling commited

--- src/components/mails-toolbar/index.js
+++ src/components/mails-toolbar/index.js
@@ -2,8 +2,15 @@ import React from "react";
 import styles from "./index.module.css";
 
 const Toolbar = () => {
-  const { toolbar } = styles;
-  return <div className={toolbar}>toolbar</div>;
+  const { toolbar, ban, trash, button } = styles;
+  return (
+    <div className={toolbar}>
+      <button className={button}>spam</button>
+      <button className={button}>del</button>
+      <button className={button}>Move To</button>
+      <button className={button}>More</button>
+    </div>
+  );
 };
 
 export default Toolbar;
