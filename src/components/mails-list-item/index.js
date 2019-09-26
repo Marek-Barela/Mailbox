33520b3852aa5b7ccac3731fb16feace0ab05189
Marek Bareła <marek.barela@wp.pl>
2019-09-26 02:46:07 +0200

    list item structure with styling commited

--- src/components/mails-list-item/index.js
+++ src/components/mails-list-item/index.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import styles from "./index.module.css";
 
 const MailsListItem = message => {
-  const { author } = message;
+  const { author, subject, shortDescription, date } = message;
+  const { listElement, checkbox, favorite, authorWrapper, subjectWrapper, messageDate } = styles;
   return (
-    <article>
-      {author}
+    <article className={listElement}>
+      <div className={checkbox}>
+        <input type="checkbox" />
+      </div>
+      <div className={favorite}> 
+        {/**ToDo icon */}
+      </div>
+      <div className={authorWrapper}>
+        <p>{author}</p>
+      </div>
+      <div className={subjectWrapper}>
+        <h6>{subject}</h6>
+        <p>{shortDescription}</p>
+      </div>
+      <div className={messageDate}>
+        {date}
+      </div>
     </article>
   )
 }
