6909d056603e420045832374b8a085b571257404
Marek Bareła <marek.barela@wp.pl>
2019-09-26 15:23:28 +0200

    mail styling update, mail structure update

--- src/components/mails-list-item/index.js
+++ src/components/mails-list-item/index.js
@@ -1,42 +1,70 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Icon from "../awesome-icon";
 import { faStar } from "@fortawesome/free-regular-svg-icons";
 import { faStar as faStarFilled } from "@fortawesome/free-solid-svg-icons";
 import styles from "./index.module.css";
 
-const MailsListItem = ({message, switchCheckbox}) => {
-  const { id, author, subject, shortDescription, date, readed, isFavorite } = message;
-  const { listElement, checkbox, favorite, authorWrapper, subjectWrapper, messageDate } = styles;
+const MailsListItem = ({ message, switchCheckbox }) => {
+  const {
+    id,
+    author,
+    subject,
+    shortDescription,
+    date,
+    readed,
+    isFavorite,
+    isChecked
+  } = message;
+  const {
+    listElement,
+    listElementActive,
+    checkbox,
+    favorite,
+    authorWrapper,
+    subjectWrapper,
+    messageDate
+  } = styles;
   const fontThickness = readed ? "400" : "800";
 
-  const [checkboxValue, setCheckboxValue] = useState(false);
+  const [checkboxValue, setCheckboxValue] = useState(isChecked);
 
   const handleCheckboxChange = () => {
     setCheckboxValue(!checkboxValue);
     // Change checkbox state in parent state
     switchCheckbox(!checkboxValue, id);
-  }
-  
+  };
+
+  const mailClassName = `${listElement} ${isChecked ? listElementActive : ""}`;
+
   return (
-    <article className={listElement}>
+    <article className={mailClassName}>
       <div className={checkbox}>
-        <input type="checkbox" value={checkboxValue} onClick={() => handleCheckboxChange()} />
+        <input
+          type="checkbox"
+          value={checkboxValue}
+          checked={checkboxValue}
+          onClick={() => handleCheckboxChange()}
+        />
       </div>
-      <div className={favorite}> 
-        { isFavorite ? <Icon icon={faStarFilled} style={{color: "#FFBD1B"}} /> : <Icon icon={faStar} /> }
+      <div className={favorite}>
+        {isFavorite ? (
+          <Icon icon={faStarFilled} style={{ color: "#FFBD1B" }} />
+        ) : (
+          <Icon icon={faStar} />
+        )}
       </div>
       <div className={authorWrapper}>
-        <p style={{fontWeight: fontThickness}}>{author}</p>
+        <p style={{ fontWeight: fontThickness }}>{author}</p>
       </div>
       <div className={subjectWrapper}>
-        <h6 style={{fontWeight: fontThickness}}>{subject}</h6>
+        <h6 style={{ fontWeight: fontThickness }}>{subject}</h6>
         <p>{shortDescription}</p>
       </div>
       <div className={messageDate}>
-        <p style={{fontWeight: fontThickness}}>{date}</p>
+        <p style={{ fontWeight: fontThickness }}>{date}</p>
       </div>
     </article>
-  )
-}
+  );
+};
 
-export default MailsListItem
+export default MailsListItem;
