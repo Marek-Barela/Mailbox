e59b19b82ee9a9deeb173232345f2a0659fcc8b4
Marek Bareła <marek.barela@wp.pl>
2019-09-26 02:54:37 +0200

    list item styling update

--- src/components/mails-list-item/index.js
+++ src/components/mails-list-item/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import styles from "./index.module.css";
 
 const MailsListItem = message => {
-  const { author, subject, shortDescription, date } = message;
+  const { author, subject, shortDescription, date, readed } = message;
   const { listElement, checkbox, favorite, authorWrapper, subjectWrapper, messageDate } = styles;
+  const fontThickness = readed ? "400" : "800";
   return (
     <article className={listElement}>
       <div className={checkbox}>
@@ -13,14 +14,14 @@ const MailsListItem = message => {
         {/**ToDo icon */}
       </div>
       <div className={authorWrapper}>
-        <p>{author}</p>
+        <p style={{fontWeight: fontThickness}}>{author}</p>
       </div>
       <div className={subjectWrapper}>
-        <h6>{subject}</h6>
+        <h6 style={{fontWeight: fontThickness}}>{subject}</h6>
         <p>{shortDescription}</p>
       </div>
       <div className={messageDate}>
-        {date}
+        <p style={{fontWeight: fontThickness}}>{date}</p>
       </div>
     </article>
   )
