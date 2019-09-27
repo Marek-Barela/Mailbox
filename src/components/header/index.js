40ab2ae54d2f6ad000173e08f35315f1ed60742d
Marek Bareła <marek.barela@wp.pl>
2019-09-27 17:10:45 +0200

    header menu button styling commited

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Icon from "../awesome-icon";
 import { faReact } from "@fortawesome/free-brands-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import styles from "./index.module.css";
 
 const Header = () => {
-  const { header, logo } = styles;
+  const { header, logo, menuButton } = styles;
   return (
     <header className={header}>
+      <button className={menuButton}>
+        <Icon icon={faBars} />
+      </button>
       <div className={logo}>
-        <Icon icon={faReact} size="2x" />
+        <Icon icon={faReact} />
         <h1>Mailbox</h1>
       </div>
     </header>
