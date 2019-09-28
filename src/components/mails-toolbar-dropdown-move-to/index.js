3db16ae12dd7681c024e98ac348a003598a654a5
Marek Bareła <marek.barela@wp.pl>
2019-09-28 22:02:44 +0200

    toolbar move-to functionality commited

--- src/components/mails-toolbar-dropdown-move-to/index.js
+++ src/components/mails-toolbar-dropdown-move-to/index.js
@@ -4,13 +4,28 @@ import {
   getBaseMessageTypes,
   getUserMessageTypes
 } from "../../selectors/getMessageTypes";
+import { changeMailsDirectoryToSpecificType } from "../../actions/toolbarActions";
+import createDashesBetweenWords from "../../utils/createDashesBetweenWords";
 import { connect } from "react-redux";
 
-const MoveToDropdown = ({ baseMessageTypes, userMessageTypes }) => {
-  console.log({ baseMessageTypes, userMessageTypes });
+const MoveToDropdown = ({ baseMessageTypes, userMessageTypes, changeMailsDirectoryToSpecificType }) => {
+  const connectedTypesOfMessages = [...baseMessageTypes, ...userMessageTypes];
+
+  const handleChangeDirectoryButton = type => changeMailsDirectoryToSpecificType(type);
+
   return (
     <DropdownWrapper>
-      <div>xd</div>
+      {connectedTypesOfMessages.map((typeOfMessage, index) => {
+        const convertedTypeOfMessage = createDashesBetweenWords(typeOfMessage).toLowerCase();
+        return (
+          <button
+            key={index}
+            onClick={() => handleChangeDirectoryButton(convertedTypeOfMessage)}
+          >
+            {typeOfMessage}
+          </button>
+        );
+      })}
     </DropdownWrapper>
   );
 };
@@ -20,7 +35,11 @@ const mapStateToProps = state => ({
   userMessageTypes: getUserMessageTypes(state)
 });
 
+const mapDispatchToProps = {
+  changeMailsDirectoryToSpecificType
+}
+
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(MoveToDropdown);
