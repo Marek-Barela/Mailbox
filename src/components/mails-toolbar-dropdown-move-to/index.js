44e48028dcfbb1327a435a439267a6370d2a3fda
Marek Bareła <marek.barela@wp.pl>
2019-09-28 21:29:22 +0200

    toolbar move-to dropdown connected with redux + basic structure

--- src/components/mails-toolbar-dropdown-move-to/index.js
+++ src/components/mails-toolbar-dropdown-move-to/index.js
@@ -1,8 +1,26 @@
 import React from "react";
 import DropdownWrapper from "../mails-toolbar-dropdown-wrapper";
+import {
+  getBaseMessageTypes,
+  getUserMessageTypes
+} from "../../selectors/getMessageTypes";
+import { connect } from "react-redux";
 
-const MoveToDropdown = () => {
-  return <DropdownWrapper>move to dropdown</DropdownWrapper>;
+const MoveToDropdown = ({ baseMessageTypes, userMessageTypes }) => {
+  console.log({ baseMessageTypes, userMessageTypes });
+  return (
+    <DropdownWrapper>
+      <div>xd</div>
+    </DropdownWrapper>
+  );
 };
 
-export default MoveToDropdown;
+const mapStateToProps = state => ({
+  baseMessageTypes: getBaseMessageTypes(state),
+  userMessageTypes: getUserMessageTypes(state)
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(MoveToDropdown);
--- src/selectors/getMessageTypes.js
+++ src/selectors/getMessageTypes.js
@@ -5,3 +5,11 @@ export const getMessageTypes = state => {
 export const getFolderInputBoolean = state => {
   return state.navbarFolders.folderInputIsDisplayed
 }
+
+export const getBaseMessageTypes = state => {
+  return state.navbarFolders.baseTypesOfMessages
+}
+
+export const getUserMessageTypes = state => {
+  return state.navbarFolders.userTypesOfMessages
+}
