a039b5b33887b82189f678c8e66c9d788ee1c54b
Marek Bareła <marek.barela@wp.pl>
2021-01-21 14:21:12 +0100

    mailbox data fetching update

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 import { FETCH_USER_DATA } from "./types";
 
-export const fetchUserData = () => async dispatch => {
+export const fetchUserData = () => async (dispatch) => {
   try {
-    const data = await axios("https://mailbox-database.herokuapp.com/api/messages").then(res => res.data.messages);
+    const data = await axios(
+      "https://mailbox-database.herokuapp.com/api/messages"
+    ).then((res) => res.data);
     dispatch({
       type: FETCH_USER_DATA,
-      payload: data
+      payload: data,
     });
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
+};
